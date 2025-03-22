ce9adc9b84c10e7956c4e67e751bd8a5050fc1ed
dilshad-debug <121053898+dilshad-debug@users.noreply.github.com>
2025-03-22 16:21:33 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -294,7 +294,7 @@ async function main() {
   });
 
   async function getSongs() {
-  let response = await fetch("/songs/audio/songs.json");
+  fetch("https://dilshad-debug.github.io/spotify/songs/audio/songs.json");
   let data = await response.json();
   
   let songs = data.songs;
